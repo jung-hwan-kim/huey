0d312604be39cfbd93c8072ab00399de6d17439c
Jung Kim <jukim@kinaxis.com>
2023-01-09 22:11:35 -0500

    :heavy_plus_sign: added few libraries

--- project.clj
+++ project.clj
@@ -3,8 +3,20 @@
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/tools.logging "1.1.0"]
                  [environ "1.2.0"]
-                 [clj-http "3.12.2"]]
-  :repl-options {:init-ns huey.core}
+                 [cheshire "5.11.0"]
+                 [org.clojure/data.csv "1.0.1"]
+                 [org.clojure/java.jdbc "0.7.12"]
+                 [org.postgresql/postgresql "42.5.0"]
+                 [clj-commons/clj-yaml "1.0.26"]
+                 [com.azure/azure-storage-blob "12.14.4"]
+                 [clojure.java-time "1.1.0"]
+                 [com.hierynomus/sshj "0.34.0"]
+                 [com.github.mwiede/jsch "0.2.6"]
+                 [io.github.nextjournal/clerk "0.12.707"]
+                 [com.google.cloud/google-cloud-storage "2.15.1"]
+                 [clj-http "3.12.3"]]
+  :repl-options {:init-ns huey.repl}
+  :java-source-paths ["main/java"]
   :resource-paths ["config", "resources"]
   :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "huey.core/run-dev"]}
                    :dependencies [[io.pedestal/pedestal.service-tools "0.5.9"]]}}
